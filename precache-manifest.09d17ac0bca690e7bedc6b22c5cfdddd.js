82e1fdfe7d7d89d8d4db73eab7774143f0fe87ef
Majid Fatahi <majid6670@gmail.com>
2018-12-26 14:17:48 +0330

    Updates

---
+++ precache-manifest.09d17ac0bca690e7bedc6b22c5cfdddd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46abc1757c42e292e5a2",
+    "url": "/reacttraining-resume/static/css/main.a2e15ffc.chunk.css"
+  },
+  {
+    "revision": "46abc1757c42e292e5a2",
+    "url": "/reacttraining-resume/static/js/main.46abc175.chunk.js"
+  },
+  {
+    "revision": "60a702564642f3aa95f4",
+    "url": "/reacttraining-resume/static/js/1.60a70256.chunk.js"
+  },
+  {
+    "revision": "730c3f3660e5153ba961",
+    "url": "/reacttraining-resume/static/js/runtime~main.730c3f36.js"
+  },
+  {
+    "revision": "a69809ac32d0302c42af617a557b319e",
+    "url": "/reacttraining-resume/index.html"
+  }
+];
